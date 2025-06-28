724039ff299ede10d15d88fef910074967a0f00d
warabimochi11 <warabimochi411@icloud.com>
2025-06-28 18:02:49 +0900

    プレイヤーの向きへ回転させるプログラム更新（途中)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,11 +15,18 @@ public class Enemy : MonoBehaviour
     /// </summary>  
     private Matrix4x4 worldMatrix_ = Matrix4x4.identity;
 
-    /// <summary>  
-    /// ターゲットとして設定する  
-    /// </summary>  
-    /// <param name="enable">true:設定する / false:解除する</param>  
-    public void SetTarget(bool enable)
+
+	/// <summary>
+	/// ターゲット
+	/// </summary>
+	[SerializeField]
+	private GameObject target_;
+
+	/// <summary>  
+	/// ターゲットとして設定する  
+	/// </summary>  
+	/// <param name="enable">true:設定する / false:解除する</param>  
+	public void SetTarget(bool enable)
     {
         // マテリアルの色を変更する  
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
@@ -38,5 +45,31 @@ public class Enemy : MonoBehaviour
     /// </summary>  
     public void Update()
     {
-    }
+
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			// ターゲットまでの向きの単位ベクトル
+			var toTarget = (target_.transform.position - transform.position).normalized;
+			// 自身の前方を指す単位ベクトル
+			var fowerd = transform.forward;
+
+			// 単位ベクトル同士の内積で cos を求める
+			var dot = Vector3.Dot(fowerd, toTarget);
+		
+
+			// cos から角度（ラジアン）を求める
+			var radian = Mathf.Acos(dot);
+
+			// 外積で回転軸を求める
+			var cross = Vector3.Cross(fowerd, toTarget);
+			// 回転軸が上向きか下向きかで角度を反転させる
+			radian *= (cross.y / Mathf.Abs(cross.y));
+
+			// 角度を指定して回転させる
+			transform.rotation *= Quaternion.Euler(0, Mathf.Rad2Deg * radian, 0);
+
+
+		}
+
+	}
 }
